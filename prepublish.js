c5f4ea8ae416ac4955006cd4e58d60fed4d10a85
bucai <1450941858@qq.com>
2021-11-11 19:54:46 +0800

    test commit husky

---
+++ prepublish.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: bucai<1450941858@qq.com>
+ * @Date: 2021-08-18 10:04:40
+ * @LastEditors: bucai<1450941858@qq.com>
+ * @LastEditTime: 2021-11-11 19:46:27
+ * @Description: 发布专用脚本
+ */
+// 流程
+// 0. 确认是否发布
+// 1. 确认版本
+// 2. eslint
+// 3. build
+// 4. publish
+// 5. commit tag
